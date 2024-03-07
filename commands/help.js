2fd8641f5b0b07d63f4d1b9a7f1fa7c392d1f1fa
Zypher <54479617+Rehpyz@users.noreply.github.com>
2024-03-07 22:25:30 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -41,7 +41,8 @@ module.exports = {
           { name: '🔄 Autoplay', value: 'Enable or disable autoplay [play random songs ]' },
           { name: '⏩ Seek', value: 'Seek to a specific time in the current song' },
           { name: '⏮️ Previous', value: 'Play the previous song in the queue' },
-          { name: '🔀 Shuffle', value: 'Shuffle the songs in queue' }
+          { name: '🔀 Shuffle', value: 'Shuffle the songs in queue' },
+          { name: '💩 Nigger', value: 'we hate niggers' },
         )
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
 
@@ -56,7 +57,7 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('https://tenor.com/view/roblox-roblox-meme-roblox-memes-boi-wtf-twerk-gif-6690897771582534374')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
         .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
